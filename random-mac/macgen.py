2af5cfce9320b79eeb74cdd122c21e9920917007
saravanan palanisamy <saravanan30erd@gmail.com>
2020-10-03 20:41:41 +0400

    support python3 - mac generator script

--- random-mac/macgen.py
+++ random-mac/macgen.py
@@ -4,4 +4,5 @@ mac = [ 0x00, 0x16, 0x3e,
 random.randint(0x00, 0x7f),
 random.randint(0x00, 0xff),
 random.randint(0x00, 0xff) ]
-print ':'.join(map(lambda x: "%02x" % x, mac))
+
+print(':'.join(map(lambda x: "%02x" % x, mac)))
